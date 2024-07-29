2c6e017192ad3265a6a7a550742ec84570223097
ngdon <ngdon@pm.me>
2024-07-29 15:32:22 -0700

    Remove mini git, icons, and statusline

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -9,25 +9,11 @@ return {
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
 
-    require('mini.git').setup()
-    require('mini.icons').setup()
-
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    -- statusline()
-    -- require('mini.surround').setup {}
-    --
-    -- -- Simple and easy statusline.
-    -- --  You could remove this setup call if you don't like it,
-    -- --  and try some other statusline plugin
-    local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
-
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
+    require('mini.surround').setup {}
   end,
 }
