e8ba9911e9cce717b7df91278fecbb206f730e32
ngdon <ngdon@pm.me>
2024-07-29 15:32:49 -0700

    move mapping

--- init.lua
+++ init.lua
@@ -161,10 +161,6 @@ vim.opt.scrolloff = 10
 --  See `:help vim.keymap.set()`
 require 'custom.mappings'
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
