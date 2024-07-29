2d1fa0ccde43a52712a12745919817d5fecbc051
ngdon <ngdon@pm.me>
2024-07-29 16:16:28 -0700

    incline update

--- lua/custom/plugins/incline.lua
+++ lua/custom/plugins/incline.lua
@@ -4,9 +4,7 @@ return {
     'SmiteshP/nvim-navic',
   },
   config = function()
-    local helpers = require 'incline.helpers'
     local devicons = require 'nvim-web-devicons'
-    local navic = require 'nvim-navic'
     require('incline').setup {
       hide = {
         focused_win = false,
@@ -22,25 +20,58 @@ return {
           filename = '[No Name]'
         end
         local ft_icon, ft_color = devicons.get_icon_color(filename)
-        local modified = vim.bo[props.buf].modified
-        local res = {
-          ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
-          ' ',
-          { filename, gui = modified and 'bold,italic' or 'bold' },
-          ' ',
-          -- guibg = '#232a2e',
-        }
-        if props.focused then
-          for _, item in ipairs(navic.get_data(props.buf) or {}) do
-            table.insert(res, {
-              { ' > ', group = 'NavicSeparator' },
-              { item.icon, group = 'NavicIcons' .. item.type },
-              { item.name, group = 'NavicText' },
-            })
+
+        local function get_git_diff()
+          local icons = { removed = ' ', changed = ' ', added = ' ' }
+          local signs = vim.b[props.buf].gitsigns_status_dict
+          local labels = {}
+          if signs == nil then
+            return labels
           end
+          for name, icon in pairs(icons) do
+            if tonumber(signs[name]) and signs[name] > 0 then
+              table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+            end
+          end
+          if #labels > 0 then
+            table.insert(labels, { '┊ ' })
+          end
+          return labels
         end
 
-        return res
+        local function get_diagnostic_label()
+          local icons = { error = ' ', warn = ' ', info = ' ', hint = '󰬏 ' }
+          local label = {}
+
+          for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if n > 0 then
+              table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+            end
+          end
+          if #label > 0 then
+            table.insert(label, { '┊ ' })
+          end
+          return label
+        end
+
+        -- Modify the filename text when unfocused.
+        local hl_group = 'Normal'
+        if not props.focused then
+          hl_group = 'Comment'
+        end
+
+        local hl_info = vim.api.nvim_get_hl(0, { name = hl_group })
+        local fg_color_raw = hl_info.fg
+        local filename_fg_color = string.format('#%06x', fg_color_raw)
+
+        return {
+          { get_diagnostic_label() },
+          { get_git_diff() },
+          { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
+          { filename .. ' ', gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold', guifg = filename_fg_color },
+          { '┊   ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
+        }
       end,
     }
   end,
