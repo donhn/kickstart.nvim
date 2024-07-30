ac203d6387779b8f945c0e1242c7d2130e2f2c27
don.nguyen <don.nguyen@astrodigital.com>
2024-07-30 09:50:21 -0700

    opts

--- lua/plugins/navic.lua
+++ lua/plugins/navic.lua
@@ -1,11 +1,9 @@
 -- A simple statusline/winbar component that uses LSP to show your current code context.
 return {
   'SmiteshP/nvim-navic',
-  config = function()
-    require('nvim-navic').setup {
-      lsp = {
-        auto_attach = true,
-      },
-    }
-  end,
+  opts = {
+    lsp = {
+      auto_attach = true,
+    },
+  },
 }
