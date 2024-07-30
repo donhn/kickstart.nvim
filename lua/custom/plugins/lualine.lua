320b6cb6400f4c7868aaa797b2145b32f4273d0f
ngdon <ngdon@pm.me>
2024-07-29 17:24:32 -0700

    update lualine to include navic

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -23,9 +23,9 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'filetype' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { 'filename', { 'navic', color_correction = 'dynamic' } },
+        lualine_x = { 'diagnostics', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
