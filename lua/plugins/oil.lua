7eaf3b0c26a4bbd28195e690c5c17a344c0349e0
don.nguyen <don.nguyen@astrodigital.com>
2024-07-30 09:50:23 -0700

    opts

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,32 +1,29 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    view_options = {
+      show_hideen = true,
+    },
+    keymaps = {
+      ['g?'] = 'actions.show_help',
+      ['<CR>'] = 'actions.select',
+      -- ["<C-s>"] = "actions.select_vsplit",
+      -- ["<C-h>"] = "actions.select_split",
+      ['<leader>ot'] = 'actions.select_tab',
+      ['<leader>op'] = 'actions.preview',
+      ['<leader>oc'] = 'actions.close',
+      ['<leader>or'] = 'actions.refresh',
+      ['-'] = 'actions.parent',
+      ['_'] = 'actions.open_cwd',
+      ['`'] = 'actions.cd',
+      ['~'] = 'actions.tcd',
+      ['gs'] = 'actions.change_sort',
+      ['gx'] = 'actions.open_external',
+      ['g.'] = 'actions.toggle_hidden',
+      ['g\\'] = 'actions.toggle_trash',
+    },
+  },
   cmd = 'Oil',
-  config = function()
-    require('oil').setup {
-      view_options = {
-        show_hideen = true,
-      },
-      keymaps = {
-        ['g?'] = 'actions.show_help',
-        ['<CR>'] = 'actions.select',
-        -- ["<C-s>"] = "actions.select_vsplit",
-        -- ["<C-h>"] = "actions.select_split",
-        ['<leader>ot'] = 'actions.select_tab',
-        ['<leader>op'] = 'actions.preview',
-        ['<leader>oc'] = 'actions.close',
-        ['<leader>or'] = 'actions.refresh',
-        ['-'] = 'actions.parent',
-        ['_'] = 'actions.open_cwd',
-        ['`'] = 'actions.cd',
-        ['~'] = 'actions.tcd',
-        ['gs'] = 'actions.change_sort',
-        ['gx'] = 'actions.open_external',
-        ['g.'] = 'actions.toggle_hidden',
-        ['g\\'] = 'actions.toggle_trash',
-      },
-    }
-  end,
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
