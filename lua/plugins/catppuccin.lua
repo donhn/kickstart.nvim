6c1062451d9c22105ed7940ff9889f82da1a0ee2
don.nguyen <don.nguyen@astrodigital.com>
2024-07-30 09:39:16 -0700

    use opt

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,29 +1,22 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
-  -- priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        treesitter = true,
-        mini = {
-          enabled = true,
-        },
-        indent_blankline = {
-          enabled = true,
-          scope_color = 'lavender', -- catppuccin color (eg. `lavender`) Default: text
-          colored_indent_levels = false,
-        },
-        which_key = true,
-        lsp_trouble = true,
+  priority = 1000,
+  opts = {
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      treesitter = true,
+      mini = {
+        enabled = true,
       },
-    }
-  end,
-  init = function()
-    -- vim.cmd.colorscheme 'catppuccin-latte'
-    vim.api.nvim_set_hl(0, 'NormalFloat', {})
-    vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
-  end,
+      indent_blankline = {
+        enabled = true,
+        scope_color = 'lavender', -- catppuccin color (eg. `lavender`) Default: text
+        colored_indent_levels = false,
+      },
+      which_key = true,
+      lsp_trouble = true,
+    },
+  },
 }
